c649862313400df936b75a5ccfed72825a758420
Paul Bokelman <paul.bokelman1@gmail.com>
2024-07-10 13:45:09 -0700

    move things

--- main.py
+++ main.py
@@ -3,24 +3,30 @@ import constants
 from lib.mc.controller import GPIO, MotorController
 from lib.mc.motor import Motor
 
+def _encoder_callback(channel):
+      """
+      Callback function for encoder
+      """
+      print("Encoder triggered")
+      print(GPIO.input(constants.encoder_pins[0]))
+
+
 def main():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(constants.encoder_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     mc = MotorController(debug=True)
 
+    GPIO.add_event_detect(constants.encoder_pins[0], GPIO.FALLING, callback=_encoder_callback, bouncetime=2)
+
     motor = Motor(1)
 
     # move each motor up and down with utils to test
     motor.set(20) # pos -> down, neg -> up
 
-    t_end = time.time() + 3
-    while time.time() < t_end:
-        print(GPIO.input(constants.encoder_pins[1]))
-
+    time.sleep(3)
     motor.stop()
 
-    mc.move_all_home()
 
 
     # move each motor up and down with utils to test

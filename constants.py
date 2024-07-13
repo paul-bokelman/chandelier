4d120679807f6068db8cc9ca1d427c8a3db98c66
Paul Bokelman <paul.bokelman1@gmail.com>
2024-07-13 14:39:16 -0700

    adjusted min speed

--- constants.py
+++ constants.py
@@ -33,5 +33,5 @@ calibrations_file_path = "calibrations.json"
 # sequence configuration
 # (max_speed + min_speed) / 2 = calibration_speed
 max_speed = 20 # relative to calibration speed
-min_speed = 2 # relative to calibration speed 
+min_speed = 6 # relative to calibration speed 
 max_counts = 30 # max encoder counts

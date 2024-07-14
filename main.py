8fa3a435539c84b3e9363c05cd4a699fbba9ad15
Paul Bokelman <paul.bokelman1@gmail.com>
2024-07-14 00:30:50 -0700

    test wave sequence

--- main.py
+++ main.py
@@ -21,10 +21,7 @@ async def main():
     # run random sequence
     sequence = Sequence()
 
-    # for positions, speed in sequence.random():
-    #     await mc.move_all(positions, speed)
-
-    for positions, speed in sequence.alternating():
+    for positions, speed in sequence.wave():
         await mc.move_all(positions, speed)
 
     # await mc.calibrate(reset=True)

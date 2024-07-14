652dd21120de06cd98ee59dea07cdcd25a07b851
Paul Bokelman <paul.bokelman1@gmail.com>
2024-07-14 00:14:46 -0700

    added params to generator type

--- lib/sequence.py
+++ lib/sequence.py
@@ -8,7 +8,7 @@ class Sequence:
     def __init__(self):
         pass
 
-    def random(self, iterations: int = 5) -> Generator[tuple[list[float], list[float]]]:
+    def random(self, iterations: int = 5) -> Generator[tuple[list[float], list[float]], None, None]:
         for _ in range(iterations):
             positions = [rand.uniform(0.5, 1) for _ in range(constants.n_motors)]
             speeds = [rand.uniform(0.4, 0.7) for _ in range(constants.n_motors)]
@@ -17,7 +17,7 @@ class Sequence:
     def wave(self, iterations: int = 5) -> Generator[tuple[list[float], list[float]]]:
         yield ([], [])
 
-    def alternating(self, iterations: int = 5, amplitude: float = 0.4, translation: float = 0.3) -> Generator[tuple[list[float], list[float]]]:
+    def alternating(self, iterations: int = 5, amplitude: float = 0.4, translation: float = 0.3) -> Generator[tuple[list[float], list[float]], None, None]:
         for i in range(iterations):
             positions = [amplitude + translation if (i + j) % 2 == 0 else translation for j in range(constants.n_motors)]
             speeds = [rand.uniform(0.4, 0.7) for _ in range(constants.n_motors)]

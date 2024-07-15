66ae2f37fa2e2e22112b4f69c86dc052be0b4a86
Paul Bokelman <paul.bokelman1@gmail.com>
2024-07-15 15:42:53 -0700

    event detect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 import constants
 import RPi.GPIO as GPIO
 # from lib.mc.controller import MotorController
@@ -14,17 +15,19 @@ async def main():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     # GPIO.setup(constants.encoder_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(constants.WSI, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(constants.WSI, GPIO.IN, )
 
-    while True: # Run forever
-        if GPIO.input(constants.WSI[0]) == GPIO.HIGH:
-            print("Wall Switch 1 is pressed")
-        if GPIO.input(constants.WSI[1]) == GPIO.HIGH:
-            print("Wall Switch 2 is pressed")
-        if GPIO.input(constants.WSI[2]) == GPIO.HIGH:
-            print("Wall Switch 3 is pressed")
-        if GPIO.input(constants.WSI[3]) == GPIO.HIGH:
-            print("Wall Switch 4 is pressed")
+    def my_callback(channel):
+        print('This is a edge event callback function!')
+        print('Edge detected on channel %s'%channel)
+        print('This is run in a different thread to your main program')
+
+    GPIO.add_event_detect(constants.WSI[0], GPIO.RISING, callback=my_callback)
+    GPIO.add_event_detect(constants.WSI[1], GPIO.RISING, callback=my_callback)
+    GPIO.add_event_detect(constants.WSI[2], GPIO.RISING, callback=my_callback)
+    GPIO.add_event_detect(constants.WSI[3], GPIO.RISING, callback=my_callback)
+
+    time.sleep(30)
 
     # mc = MotorController(debug=True)
     # await mc.move_all(0.5)

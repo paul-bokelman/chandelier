8e0de3ff54710d3721679349902f695761ee8734
Paul Bokelman <paul.bokelman1@gmail.com>
2024-07-15 15:45:41 -0700

    test

--- main.py
+++ main.py
@@ -15,19 +15,14 @@ async def main():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     # GPIO.setup(constants.encoder_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(constants.WSI, GPIO.IN, )
+    # GPIO.setup(ledpin, GPIO.OUT) # set ledpin as an output
+    GPIO.setup(constants.WSI[0], GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # with pull up resistor
 
-    def my_callback(channel):
-        print('This is a edge event callback function!')
-        print('Edge detected on channel %s'%channel)
-        print('This is run in a different thread to your main program')
+    while True:
+        # GPIO.output(ledpin, GPIO.input(pushpin))
+        if GPIO.input(constants.WSI[0]) == GPIO.HIGH:
+            print('pressed')
 
-    GPIO.add_event_detect(constants.WSI[0], GPIO.RISING, callback=my_callback)
-    GPIO.add_event_detect(constants.WSI[1], GPIO.RISING, callback=my_callback)
-    GPIO.add_event_detect(constants.WSI[2], GPIO.RISING, callback=my_callback)
-    GPIO.add_event_detect(constants.WSI[3], GPIO.RISING, callback=my_callback)
-
-    time.sleep(30)
 
     # mc = MotorController(debug=True)
     # await mc.move_all(0.5)

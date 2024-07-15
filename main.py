f3eb94cd7550175f1182f8a7eddb9c18b9220a72
Paul Bokelman <paul.bokelman1@gmail.com>
2024-07-15 15:56:45 -0700

    more input

--- main.py
+++ main.py
@@ -13,13 +13,26 @@ import RPi.GPIO as GPIO
 async def main():
     # setup GPIO
     GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
     # GPIO.setup(constants.encoder_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     # GPIO.setup(ledpin, GPIO.OUT) # set ledpin as an output
     GPIO.setup(constants.led_pin, GPIO.OUT) # set ledpin as an output
-    GPIO.setup(constants.service_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set service_button_pin as an input
+    GPIO.setup(constants.service_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(constants.reboot_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(constants.wall_switch_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     # turn on led
+
+    def button_callback(channel):
+        print(f"Button {channel} was pushed!")
+
+    GPIO.add_event_detect(constants.service_button_pin, GPIO.FALLING, callback=button_callback, bouncetime=300)
+
+    GPIO.add_event_detect(constants.reboot_button_pin, GPIO.FALLING, callback=button_callback, bouncetime=300)
+
+    GPIO.add_event_detect(constants.wall_switch_pins[0], GPIO.FALLING, callback=button_callback, bouncetime=300)
+
+    GPIO.add_event_detect(constants.wall_switch_pins[1], GPIO.FALLING, callback=button_callback, bouncetime=300)
+
 
     while True:
         GPIO.output(constants.led_pin, GPIO.HIGH)

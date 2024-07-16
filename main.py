1fb2ce1b5d22fac01fcf811aa04e79ef062bf687
Paul Bokelman <paul.bokelman1@gmail.com>
2024-07-16 14:18:42 -0700

    installed schedule and removed switch tests

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import asyncio
 import time
-import constants
+import schedule
 import RPi.GPIO as GPIO
-# from lib.mc.controller import MotorController
-# from lib.sequence import Sequence
+import constants
+from lib.mc.controller import MotorController
+from lib.sequence import Sequence
 
 # async def idle_state(mc: MotorController):
 #     """Idle state"""
@@ -13,55 +14,22 @@ import RPi.GPIO as GPIO
 async def main():
     # setup GPIO
     GPIO.setmode(GPIO.BCM)
-    # GPIO.setup(constants.encoder_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    # GPIO.setup(ledpin, GPIO.OUT) # set ledpin as an output
-    GPIO.setup(constants.led_pin, GPIO.OUT) # set ledpin as an output
-    GPIO.setup(constants.service_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(constants.reboot_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(constants.wall_switch_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(constants.encoder_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    # turn on led
-
-    def button_callback(channel):
-        print(f"Button {channel} was pushed!")
-        GPIO.output(constants.led_pin, GPIO.HIGH)
-
-    def wall_switch_callback(channel):
-        print(f"Wall switch {channel} was pushed!")
-        GPIO.output(constants.led_pin, GPIO.HIGH)
-
-    GPIO.add_event_detect(constants.service_button_pin, GPIO.FALLING, callback=button_callback, bouncetime=300)
-    GPIO.add_event_detect(constants.reboot_button_pin, GPIO.FALLING, callback=button_callback, bouncetime=300)
-    GPIO.add_event_detect(constants.wall_switch_pins[0], GPIO.FALLING, callback=wall_switch_callback, bouncetime=300)
-    GPIO.add_event_detect(constants.wall_switch_pins[1], GPIO.FALLING, callback=wall_switch_callback, bouncetime=300)
-
-    while True:
-        GPIO.output(constants.led_pin, GPIO.HIGH)
-        time.sleep(0.5)
-        GPIO.output(constants.led_pin, GPIO.LOW)
-        time.sleep(0.5)
-
-
-
-    # mc = MotorController(debug=True)
-    # await mc.move_all(0.5)
-
-    # await mc.move_all_home()
+    mc = MotorController(debug=True)
+    await mc.move_all_home()
 
     # run random sequence
-    # sequence = Sequence()
+    sequence = Sequence()
 
-    # for positions, speed in sequence.random():
-    #     await mc.move_all(positions, speed)
+    for positions, speed in sequence.random():
+        await mc.move_all(positions, speed)
 
     # await mc.calibrate(reset=True)
 
-    # await mc.move_all([0.5, 0.5, 0.5, 0.5], 0.5)
-    # await mc.move_all_home()
-    
-    # mc.stop_all_motors()
+    mc.stop_all_motors()
 
-    # mc.save_calibration()
+    mc.save_calibration()
 
     GPIO.cleanup() # clean up for next session
 

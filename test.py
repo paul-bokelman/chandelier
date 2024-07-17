334eb7f4dd147e04667ec505f0f7aa4d7f3b7983
Paul Bokelman <paul.bokelman1@gmail.com>
2024-07-17 16:23:34 -0700

    changed min and max pulse

--- test.py
+++ test.py
@@ -9,4 +9,5 @@ kit.continuous_servo[2].throttle = 1
 time.sleep(1)
 kit.continuous_servo[2].throttle = -1
 time.sleep(1)
+kit.continuous_servo[2].set_pulse_width_range(1000, 2000)
 kit.continuous_servo[2].throttle = 0

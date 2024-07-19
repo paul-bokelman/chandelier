ad45f51a91ea635980f4213b3d37ae1e5e2f04c8
Paul Bokelman <paul.bokelman1@gmail.com>
2024-07-19 16:00:39 -0700

    added speeds back to move all

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ async def main():
     mc = MotorController()
     await mc.calibrate()
     await mc.move_all_home()
-    await mc.move_all(0.3, [0.3870312500000002, 0.9100000000000006, 0.2, 0.28])
+    await mc.move_all(0.5, [0.3870312500000002, 0.9100000000000006, 0.2, 0.28])
 
 
     # M3 -> 0.28

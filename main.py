1c35b1d7255aa42d896214f951025167e5d6361d
Paul Bokelman <paul.bokelman1@gmail.com>
2024-07-28 15:33:27 -0700

    matching throttle

--- main.py
+++ main.py
@@ -135,9 +135,9 @@ async def main():
     await mc.calibrate()
     # await mc.move_all([0.5, 0.5], [0.38, 0.28])
 
-    # timed_out, time_elapsed = await mc.motors[2].move(constants.calibration_counts, throttle=0.6850000000000003)
+    timed_out, time_elapsed = await mc.motors[2].move(constants.calibration_counts, throttle=0.38)
 
-    timed_out, time_elapsed = await mc.motors[3].move(constants.calibration_counts, throttle=constants.ThrottlePresets.SLOW, direction=constants.down)
+    # timed_out, time_elapsed = await mc.motors[3].move(constants.calibration_counts, throttle=constants.ThrottlePresets.SLOW, direction=constants.down)
 
 
     print(constants.calibration_counts / time_elapsed, "=? 2.1271938266148247")

d9f65d00f235a33be0e4836388900e252a26d9d3
Paul Bokelman <paul.bokelman1@gmail.com>
2024-08-06 22:12:43 -0700

    cleanup fallback

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import constants
 
 
 async def main():
-    # GPIO setup
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(constants.encoder_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(constants.service_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(constants.reboot_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(constants.wall_switch_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(constants.led_pin, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.cleanup() # initial clean up to avoid any issues
+    try: 
+        # set up GPIO
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(constants.encoder_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(constants.service_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(constants.reboot_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(constants.wall_switch_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(constants.led_pin, GPIO.OUT, initial=GPIO.LOW)
 
     # sm = StateMachine()
 
@@ -28,5 +30,15 @@ async def main():
 
     # GPIO.cleanup() # clean up for next session
 
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        GPIO.cleanup()
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Interrupted by user")
+        GPIO.cleanup()
+        exit()

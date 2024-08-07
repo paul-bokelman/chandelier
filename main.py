34eb6aeb2a603d26a374d771265c5e8086c32af2
Paul Bokelman <paul.bokelman1@gmail.com>
2024-08-06 22:48:10 -0700

    enable pins

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ async def main():
         GPIO.setup(constants.encoder_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(constants.service_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(constants.wall_switch_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # GPIO.setup(constants.led_pin, GPIO.OUT, initial=GPIO.LOW)
-        # GPIO.setup(constants.reboot_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(constants.led_pin, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(constants.reboot_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
         sm = StateMachine()
 
--- test.py
+++
@@ -1,8 +0,0 @@
-import RPi.GPIO as GPIO
-
-try: 
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
-
-finally:
-    GPIO.cleanup()

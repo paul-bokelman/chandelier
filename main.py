b0d1ef1c8d256b6e673f69396df187f3033e918f
Paul Bokelman <paul.bokelman1@gmail.com>
2024-08-07 19:00:28 -0700

    test pud up

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ async def main():
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(constants.encoder_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(constants.service_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(constants.wall_switch_pins, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(constants.wall_switch_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(constants.led_pin, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(constants.reboot_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 

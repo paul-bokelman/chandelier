75a214f3840acef909e13fade6fc43ff1ce7f191
Paul Bokelman <paul.bokelman1@gmail.com>
2024-08-08 15:21:34 -0700

    updated boot

--- boot.sh
+++ boot.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 cd /home/chandelier2/Desktop/Chandelier
+
+# update the code to latest
+git restore .
+git pull
 source ./env/bin/activate
 python ./main.py
--- lib/controller.py
+++ lib/controller.py
@@ -1,4 +1,4 @@
-from typing import Union, Optional
+from typing import Union
 from lib.motor import Throttle
 import asyncio
 import constants

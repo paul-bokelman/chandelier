3bd6abb30caed543a0f10337a739a3ceb971e603
Paul Bokelman <paul.bokelman1@gmail.com>
2024-08-07 20:27:04 -0700

    extend to active motor length

--- lib/sequence.py
+++ lib/sequence.py
@@ -13,7 +13,7 @@ class Sequence:
 
     def random_iteration(self) -> tuple[list[float], Throttle]:
         """Generate a random sequence for all motors"""
-        positions = [rand.uniform(0.5, 1) for _ in range(constants.n_motors)]
+        positions = [rand.uniform(0.5, 1) for _ in range(constants.n_active_motors)]
         throttles = self.default_throttles
         return (positions, throttles)
 
@@ -25,14 +25,14 @@ class Sequence:
     def wave(self, iterations: int = 5, amplitude: float = 0.4, translation: float = 0.3, frequency: float = 0.5, step: float = 0.5) -> GeneratedSequence:
         """Generate a wave sequence for all motors"""
         for i in range(iterations):
-            positions = [amplitude * math.sin(2 * math.pi * frequency * i + step * j) + translation + amplitude for j in range(constants.n_motors)]
+            positions = [amplitude * math.sin(2 * math.pi * frequency * i + step * j) + translation + amplitude for j in range(constants.n_active_motors)]
             throttles = self.default_throttles
             yield (positions, throttles)
 
     def alternating(self, iterations: int = 5, amplitude: float = 0.4, translation: float = 0.4) -> GeneratedSequence:
         """Generate an alternating sequence for all motors"""
         for i in range(iterations):
-            positions = [amplitude + translation if (i + j) % 2 == 0 else translation for j in range(constants.n_motors)]
+            positions = [amplitude + translation if (i + j) % 2 == 0 else translation for j in range(constants.n_active_motors)]
             throttles = self.default_throttles
             yield (positions, throttles)
 

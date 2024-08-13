fbb3170267a106921579e3e367ffc86416c8f510
Paul Bokelman <paul.bokelman1@gmail.com>
2024-08-12 22:27:45 -0700

    remove stop all fallback

--- blank-test.py
+++ blank-test.py
@@ -5,7 +5,6 @@ from lib.controller import MotorController
 from lib.utils import log
 
 async def blank_test():
-    mc = MotorController()
     try: 
         # set up GPIO pins
         GPIO.setmode(GPIO.BCM)
@@ -15,6 +14,7 @@ async def blank_test():
         GPIO.setup(constants.led_pin, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(constants.charging_pin, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(constants.reboot_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        mc = MotorController()
 
         await mc.move_all(0.9, 0.5)
         await mc.move_all_home(-0.2)
@@ -22,10 +22,8 @@ async def blank_test():
         mc.stop_all_motors()
 
     except Exception as e:
-        mc.stop_all_motors()
         log.error(f"An error occurred: {e}")
     finally:
-        mc.stop_all_motors()
         GPIO.cleanup()
 
 asyncio.run(blank_test())

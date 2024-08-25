5103fbe7b3655df4e39451036e17c28c992eb2af
Paul Bokelman <paul.bokelman1@gmail.com>
2024-08-24 20:52:56 -0600

    test without args

--- main.py
+++ main.py
@@ -27,36 +27,36 @@ def main():
         GPIO.setup(constants.reboot_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
         # blank test mode
-        if args.mode == "blank":
-            log.info("Running blank test mode")
-            mc = MotorController()
-            asyncio.run(mc.move_all_home(-0.2))
-            asyncio.run(mc.move_all(0.1, 0.5))
-            asyncio.run(mc.move_all_home(-0.2))
-            mc.stop_all_motors()
-            log.info("Blank test mode complete")
-        # encoders test mode
-        elif args.mode == "encoders":
-            log.info("Running encoders test mode")
-            mc = MotorController()
-            asyncio.run(mc.move_all_home(-0.2))
-            asyncio.run(mc.move_all(0.1, 0.5))
-            asyncio.run(mc.move_all_home(-0.2))
-            mc.stop_all_motors()
+        # if args.mode == "blank":
+        #     log.info("Running blank test mode")
+        #     mc = MotorController()
+        #     asyncio.run(mc.move_all_home(-0.2))
+        #     asyncio.run(mc.move_all(0.1, 0.5))
+        #     asyncio.run(mc.move_all_home(-0.2))
+        #     mc.stop_all_motors()
+        #     log.info("Blank test mode complete")
+        # # encoders test mode
+        # elif args.mode == "encoders":
+        #     log.info("Running encoders test mode")
+        #     mc = MotorController()
+        #     asyncio.run(mc.move_all_home(-0.2))
+        #     asyncio.run(mc.move_all(0.1, 0.5))
+        #     asyncio.run(mc.move_all_home(-0.2))
+        #     mc.stop_all_motors()
 
-            disabled_motors = [m for m in mc.motors if m.disabled]
+        #     disabled_motors = [m for m in mc.motors if m.disabled]
 
-            if len(disabled_motors) > 0:
-                log.error(f"Disabled motors: {disabled_motors}")
-            else:
-                log.info("All motors are enabled")
-        # normal operation mode
-        else:
-            sm = StateMachine()
+        #     if len(disabled_motors) > 0:
+        #         log.error(f"Disabled motors: {disabled_motors}")
+        #     else:
+        #         log.info("All motors are enabled")
+        # # normal operation mode
+        # else:
+        sm = StateMachine()
 
-            # run state machine forever
-            while True:
-                asyncio.run(sm.check())
+        # run state machine forever
+        while True:
+            asyncio.run(sm.check())
 
     except Exception as e:
         log.error(f"An error occurred: {e}")

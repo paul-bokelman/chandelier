761e4d8e3e5380822f2269a5b470e909e8d377dd
Paul Bokelman <paul.bokelman1@gmail.com>
2024-08-29 01:16:09 -0600

    update boot script

--- boot.sh
+++ boot.sh
@@ -3,8 +3,8 @@
 cd /home/kevin/Desktop/Chandelier
 
 # update the code to latest
-git restore .
-git pull
+# git restore .
+# git pull
 source ./env/bin/activate
 python ./main.py -m=auto
 # python ./blank-test.py

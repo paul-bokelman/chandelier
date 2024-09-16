602357e6c5def7685b8b1ce9bf113c055e334954
Paul Bokelman <paul.bokelman1@gmail.com>
2024-09-15 18:15:55 -0600

    updated scripts

--- kill.sh
+++ kill.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo ps -ax | pgrep -f 'python ./main.py' | xargs sudo kill
-cd /home/kevin/Desktop/Chandelier
+cd /home/chandelier2/Desktop/Chandelier
 source ./env/bin/activate
 python ./helper-scripts/stop.py

35f2eaffb89d347f03ae4b29e68e0619304350ae
Paul Bokelman <paul.bokelman1@gmail.com>
2024-10-08 14:12:04 -0600

    ui (#12)

--- modes/manual.py
+++ modes/manual.py
@@ -3,7 +3,17 @@ from lib.state import StateMachine
 from lib.utils import log
 
 def run():
-    log.info("Running manual mode")
+    log.info("""
+    Running manual mode
+    Controls (press button): 
+        i: idle state
+        r: random state
+        s: sequence state
+        c: service state
+        q: reboot
+    """)
+
+
     sm = StateMachine(manual=True)
 
     # run state machine forever
